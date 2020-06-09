c1f3b5c1433a576da85c60fe02df741e39c0d3aa
John Pham <mr.john.pham@gmail.com>
2020-06-09 02:26:10 -0400

    Configure webpack with style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html")
         })
-    ]
+    ],
+
 };
