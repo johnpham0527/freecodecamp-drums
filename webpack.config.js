211dfb3af1f9028239ccd44e44f38e772dfd91d6
John Pham <mr.john.pham@gmail.com>
2020-06-09 02:43:13 -0400

    Configure webpack for jsx

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
                 use: ["style-loader", "css-loader"]
             },
             {
-                test: /\.js$/,
+                test: /\.js|jsx$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
             }
