c3d86afd0eb27616d4e69f886e8ad69b8a2d5921
John Pham <mr.john.pham@gmail.com>
2020-06-10 05:43:12 -0400

    Rename letter

--- src/js/components/Drums.js
+++ src/js/components/Drums.js
@@ -8,26 +8,26 @@ class Button extends Component {
   }
 
   handleClick() {
-    let sound = document.getElementById(this.props.capitalLetter)
+    let sound = document.getElementById(this.props.letter)
     sound.play()
   }
 
   render() {
     return (
       <button class='drum-pad'>
-        {this.props.capitalLetter}
-        <audio class='clip' id={this.props.capitalLetter} src={this.props.audioSource} onClick={this.handleClick}></audio>
+        {this.props.letter}
+        <audio class='clip' id={this.props.letter} src={this.props.audioSource} onClick={this.handleClick}></audio>
       </button>
     )
   }
 }
 
 /*
-const Button = ({capitalLetter, audioSource, handleDrumClick}) => { //destructuring props
+const Button = ({letter, audioSource, handleDrumClick}) => { //destructuring props
   return (
     <button class='drum-pad'>
-      {capitalLetter}
-      <audio class='clip' id={capitalLetter} src={audioSource} onClick={handleDrumClick}></audio>
+      {letter}
+      <audio class='clip' id={letter} src={audioSource} onClick={handleDrumClick}></audio>
     </button>
   )
 }
@@ -36,20 +36,13 @@ const Button = ({capitalLetter, audioSource, handleDrumClick}) => { //destructur
 class Drums extends Component {
   constructor() {
     super();
-
-    this.state = {
-    };
-
-    this.handleDrumClick = this.handleDrumClick.bind(this);
   }
 
-  handleDrumClick() {
-  }
 
   render() {
     return (
       <div>
-        <Button capitalLetter='Q' audioSource='http://john-pham.com/freecodecamp/BingBaa.mp3' handleDrumClick={this.handleDrumClick}></Button>
+        <Button letter='Q' audioSource='http://john-pham.com/freecodecamp/BingBaa.mp3'></Button>
       </div>
     );
   }
