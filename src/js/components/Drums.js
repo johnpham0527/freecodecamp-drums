4a8cb778229c9e2b236341c429861ddfec7d164f
John Pham <mr.john.pham@gmail.com>
2020-06-10 05:53:25 -0400

    Change playAudio method to handleClick

--- src/js/components/Drums.js
+++ src/js/components/Drums.js
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+/*
+const playAudio = letter => {
+    let sound = document.getElementById(letter)
+    sound.play()
+}
+*/
+
 class Button extends Component {
   constructor(props) {
     super();
-    this.playAudio = this.playAudio.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  playAudio() {
+  handleClick() {
     let sound = document.getElementById(this.props.letter)
     sound.play()
   }
 
   render() {
     return (
-      <button class='drum-pad' onClick={this.playAudio} type='button'>
+      <button class='drum-pad' onClick={this.handleClick} type='button'>
         {this.props.letter}
         <audio class='clip' id={this.props.letter} src={this.props.audioSource} type='audio/mpeg'></audio>
       </button>
@@ -30,7 +37,7 @@ class Drums extends Component {
   }
 
   handleKeyPress(event) {
-    
+
   }
 
   render() {
