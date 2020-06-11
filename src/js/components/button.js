f291bd2be05431ec736714c0df32ea2248d296e4
John Pham <mr.john.pham@gmail.com>
2020-06-11 05:18:17 -0400

    Remove unneeded comments

--- src/js/components/button.js
+++ src/js/components/button.js
@@ -1,18 +1,9 @@
 import React, { Component } from "react";
-import playAudio from "./audio";
 
 class Button extends Component {
     constructor({props}) {
       super(props);
-      //this.handleClick = this.handleClick.bind(this);
     }
-  
-    /*
-    handleClick() {
-      playAudio(this.props.letter);
-    }
-    */
-    
   
     render() {
       const { letter, audioSource, id, clickHandler } = this.props; //destructure props
