79f6679495f7d98bd9f9a1864a94b3d75907adea
John Pham <mr.john.pham@gmail.com>
2020-06-11 03:21:37 -0400

    Move drumsList and ReactDOM.render into index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
 import "./css/style.css";
-import Drums from "./js/components/Drums";
+import React from "react";
+import ReactDOM from "react-dom";
+import DrumsApp from "./js/components/drums";
+
+const drumsList = {
+    '81': {
+      id: 'Q',
+      src: 'http://john-pham.com/freecodecamp/BingBaa.mp3'
+    },
+    '87': {
+      id: 'W',
+      src: 'http://john-pham.com/freecodecamp/Celebration.mp3'
+    },
+    '69': {
+      id: 'E',
+      src: 'http://john-pham.com/freecodecamp/Organic_cloudcity.mp3'
+    },
+    '65': {
+      id: 'A',
+      src: 'http://john-pham.com/freecodecamp/Organic_miniminuet.mp3'
+    },
+    '83': {
+      id: 'S',
+      src: 'http://john-pham.com/freecodecamp/Organic_plucking.mp3'
+    },
+    '68': {
+      id: 'D',
+      src: 'http://john-pham.com/freecodecamp/Organic_saasy.mp3'
+    },
+    '90': {
+      id: 'Z',
+      src: 'http://john-pham.com/freecodecamp/Organic_xylobone.mp3'
+    },
+    '88': {
+      id: 'X',
+      src: 'http://john-pham.com/freecodecamp/Shred1.mp3'
+    },
+    '67': {
+      id: 'C',
+      src: 'http://john-pham.com/freecodecamp/Shred2.mp3'
+    }
+  }
+
+const wrapper = document.getElementById("root");
+wrapper ? ReactDOM.render(<DrumsApp drums={drumsList}/>, wrapper) : false;
--- src/js/components/Drums.js
+++ src/js/components/Drums.js
@@ -1,44 +1,4 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
-
-const drumsList = {
-  '81': {
-    id: 'Q',
-    src: 'http://john-pham.com/freecodecamp/BingBaa.mp3'
-  },
-  '87': {
-    id: 'W',
-    src: 'http://john-pham.com/freecodecamp/Celebration.mp3'
-  },
-  '69': {
-    id: 'E',
-    src: 'http://john-pham.com/freecodecamp/Organic_cloudcity.mp3'
-  },
-  '65': {
-    id: 'A',
-    src: 'http://john-pham.com/freecodecamp/Organic_miniminuet.mp3'
-  },
-  '83': {
-    id: 'S',
-    src: 'http://john-pham.com/freecodecamp/Organic_plucking.mp3'
-  },
-  '68': {
-    id: 'D',
-    src: 'http://john-pham.com/freecodecamp/Organic_saasy.mp3'
-  },
-  '90': {
-    id: 'Z',
-    src: 'http://john-pham.com/freecodecamp/Organic_xylobone.mp3'
-  },
-  '88': {
-    id: 'X',
-    src: 'http://john-pham.com/freecodecamp/Shred1.mp3'
-  },
-  '67': {
-    id: 'C',
-    src: 'http://john-pham.com/freecodecamp/Shred2.mp3'
-  }
-}
 
 const playAudio = letter => {
     let sound = document.getElementById(letter);
@@ -106,6 +66,3 @@ class DrumsApp extends Component {
 }
 
 export default DrumsApp;
-
-const wrapper = document.getElementById("root");
-wrapper ? ReactDOM.render(<DrumsApp drums={drumsList}/>, wrapper) : false;
