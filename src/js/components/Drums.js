aa58bf5158790cf0a92592e2122f301eece2346d
John Pham <mr.john.pham@gmail.com>
2020-06-11 03:09:03 -0400

    Populate drumsList with all nine drums

--- src/js/components/Drums.js
+++ src/js/components/Drums.js
@@ -9,6 +9,34 @@ const drumsList = {
   '87': {
     id: 'W',
     src: 'http://john-pham.com/freecodecamp/Celebration.mp3'
+  },
+  '69': {
+    id: 'E',
+    src: 'http://john-pham.com/freecodecamp/Organic_cloudcity.mp3'
+  },
+  '65': {
+    id: 'A',
+    src: 'http://john-pham.com/freecodecamp/Organic_miniminuet.mp3'
+  },
+  '83': {
+    id: 'S',
+    src: 'http://john-pham.com/freecodecamp/Organic_plucking.mp3'
+  },
+  '68': {
+    id: 'D',
+    src: 'http://john-pham.com/freecodecamp/Organic_saasy.mp3'
+  },
+  '90': {
+    id: 'Z',
+    src: 'http://john-pham.com/freecodecamp/Organic_xylobone.mp3'
+  },
+  '88': {
+    id: 'X',
+    src: 'http://john-pham.com/freecodecamp/Shred1.mp3'
+  },
+  '67': {
+    id: 'C',
+    src: 'http://john-pham.com/freecodecamp/Shred2.mp3'
   }
 }
 
