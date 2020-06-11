bea90663616abe18ccecebf9a9621438e93f0ce2
John Pham <mr.john.pham@gmail.com>
2020-06-11 03:29:05 -0400

    Move Button into its own file

--- src/js/components/Drums.js
+++ src/js/components/Drums.js
@@ -1,26 +1,6 @@
 import React, { Component } from "react";
 import playAudio from "./play-audio";
-
-class Button extends Component {
-  constructor({props}) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    playAudio(this.props.letter);
-  }
-
-  render() {
-    const { letter, audioSource } = this.props; //destructure props
-    return (
-      <button className='drum-pad' onClick={this.handleClick} type='button'>
-        {letter}
-        <audio className='clip' id={letter} src={audioSource} type='audio/mpeg'></audio>
-      </button>
-    )
-  }
-}
+import Button from "./button";
 
 class DrumsApp extends Component {
   constructor(props) {
